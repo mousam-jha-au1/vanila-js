13ef4a10b614072acc985e247e099d3a995f9b24
mousam-jha-au1 <jhamousam@gmail.com>
2020-09-06 21:23:13 +0530

    Go-Corona Initial Push

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # JavaScript Projects
 
 ## 1. Days-left-for-2021
+
+- App that shows days, hours, minutes and seconds left for 1st Jan 2021.
---
+++ go-corona-quiz-app/js/script.js
@@ -0,0 +1,8 @@
+// DOM elements
+const question = document.querySelector(".question");
+const options = document.querySelectorAll(".options");
+const button = document.querySelector(".btn");
+
+button.addEventListener("click", function () {
+  console.log("hello there!");
+});
